4701d3d124b393e7f88367e5d559c9f7762a9e37
DietCokeOfEvil <dietcokeofevil@server.fake>
2013-07-12 01:06:12 -0400

    Reformulated shrinkwrap to expose the centering and scaling operations as separate JQuery plugins.  Also removed code to automatically re-invoke shrinkwrap on resize; the calling page can now choose to do this or not.

--- jquery.shrinkwrap.js
+++ jquery.shrinkwrap.js
@@ -19,51 +19,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * Shrinkwrap: scales and centers an image inside its container's box.
+ * 
+ * Scaling preserves aspect ratio.
+ * Centering is both horizontal and vertical.
+ */
 (function($) {
     $.fn.extend({
         shrinkWrap: function() {
+            return $(this).fitToParentBox().centerInParentBox();
+        }
+    });
+})(jQuery);
+
+/**
+ * Fits an item inside its container's box.  Scaling preserves aspect ratio.
+ */
+(function($) {
+    $.fn.extend({
+        fitToParentBox: function() {
             return this.each(function() {
                 var item = $(this);
                 var box = $(this).parent();
-                _shrinkWrap(item, box);
                 
-                $(window).resize(function() {_shrinkWrap(item, box);})
+                // Figure out which dimension needs to be scaled the most.
+                var widthDelta = box.width() - item.width();
+                var heightDelta = box.height() - item.height();
+
+                newWidth = item.width();
+                newHeight = item.height();
+                if (widthDelta > heightDelta) {
+                    newWidth = 'auto';
+                    newHeight = '100%';
+                }
+                else {
+                    newWidth = '100%';
+                    newHeight = 'auto';
+                }
+
+                item.css('position', 'relative');
+                item.css('width', newWidth);
+                item.css('height', newHeight);
             });
         }
     });
 })(jQuery);
 
-function _shrinkWrap(item, box) {
-    _fitToBoundingBox(item, box);
-    _centerInBox(item, box);
-}
+/**
+ * Centres an item inside its container's box.
+ */
+(function($) {
+    $.fn.extend({
+        centerInParentBox: function() {
+            return this.each(function() {
+                var item = $(this);
+                var box = $(this).parent();
+                
+                var widthDelta = box.width() - item.width();
+                var heightDelta = box.height() - item.height();
 
-function _fitToBoundingBox(item, box) {
-    // Figure out which dimension needs to be scaled the most.
-    var widthDelta = box.width() - item.width();
-    var heightDelta = box.height() - item.height();
-    
-    newWidth = item.width();
-    newHeight = item.height();
-    if (widthDelta > heightDelta) {
-        newWidth = 'auto';
-        newHeight = '100%';
-    }
-    else {
-        newWidth = '100%';
-        newHeight = 'auto';
-    }
-    
-    item.css('position', 'relative');
-    item.css('width', newWidth);
-    item.css('height', newHeight);
-}
-
-function _centerInBox(item, box) {
-    var widthDelta = box.width() - item.width();
-    var heightDelta = box.height() - item.height();
-
-    item.css('position', 'relative');
-    item.css('left', (widthDelta / 2) + 'px');
-    item.css('top', (heightDelta / 2)  + 'px');
-}
+                item.css('position', 'relative');
+                item.css('left', (widthDelta / 2) + 'px');
+                item.css('top', (heightDelta / 2)  + 'px');
+            });
+        }
+    })
+})(jQuery);
